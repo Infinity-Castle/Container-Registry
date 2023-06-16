794ec414a80e1d01adef7093653bca4134a37de6
UtkarshM-Hub <utmandape4@gmail.com>
2023-06-16 23:44:15 +0530

    Feat: check

--- .github/scripts/linuxdiary-4.0/linuxdiary-4.0.py
+++ .github/scripts/linuxdiary-4.0/linuxdiary-4.0.py
@@ -1,45 +1,46 @@
-import os
 import hashlib
+from importlib.resources import path
+import os
+import sys
+import pathlib as Path
+from xml.etree.ElementTree import tostring
 
-directory = os.environ['GITHUB_WORKSPACE'] + "/wargames4.0"
-updateFile = None
-
-def check(hash,dockerfile):
-    print("Checking " + dockerfile,end='')
+def calculate_sha256(file_path):
     sha256_hash = hashlib.sha256()
+    with open(file_path, "rb") as file:
+        for chunk in iter(lambda: file.read(4096), b""):
+            sha256_hash.update(chunk)
+    return sha256_hash.hexdigest()
 
-    with open(dockerfile,"rb") as f:
-        for byte_block in iter(lambda: f.read(4096),b""):
-            sha256_hash.update(byte_block)
-    
-    if(not os.path.exists(hash)):
-        open(hash, 'w+')
-    
-    with open(hash, 'r') as file:
-        fileHash = file.read().replace('\n', '')
+def update_hash_file(file_path, hash_value):
+    with open(file_path, "w") as file:
+        file.write(hash_value)
 
-        fileHash = fileHash.split(' ')[0]
+def store_changed_dockerfile(file_path, dockerfile_location):
+    with open(file_path, "a") as file:
+        file.write(dockerfile_location + "\n")
 
-        if(fileHash==sha256_hash.hexdigest()):
-            print(" - Not Modified")
-            return 
-        else:
-            print(" - Modified")
-            updateFile.write(dockerfile+" "+hash+"\n")
+def check_dockerfile_hashes(directory, changed_dockerfiles_file):
+    for root, dirs, files in os.walk(directory):
+        if "Dockerfile" in files and "hashfile" in files:
+            dockerfile_path = os.path.join(root, "Dockerfile")
+            hashfile_path = os.path.join(root, "hashfile")
 
-def process(parent_dir, file):
-    for filename in os.listdir(file):
-        f = os.path.join(file, filename)
-        if not os.path.isfile(f):
-            process(file, f)
-        elif filename=="Dockerfile":
-            check(file+"/hash", f)
+            existing_hash = None
+            with open(hashfile_path, "r") as file:
+                existing_hash = file.read().strip()
 
-def main():
-    global updateFile
-    updateFile = open(os.environ['GITHUB_WORKSPACE'] + "/wargames4.0/Dockerfile", "w+")
-    process(directory, directory)
-    updateFile.close()
+            current_hash = calculate_sha256(dockerfile_path)
 
-if __name__=="__main__":
-    main()
+            if existing_hash == current_hash:
+                print(f"Dockerfile in {root} has not changed.")
+            else:
+                print(f"Dockerfile in {root} has been updated.")
+                update_hash_file(hashfile_path, current_hash)
+                store_changed_dockerfile(changed_dockerfiles_file, dockerfile_path)
+
+# Specify the root directory where Dockerfiles are located
+root_directory = os.environ['GITHUB_WORKSPACE'] + "/wargames4.0"
+# Specify the path to the text file to store changed Dockerfile locations
+changed_dockerfiles_file = os.environ['GITHUB_WORKSPACE'] + "/.github/updates/linuxdiary-4.0.txt"
+# check_dockerfile_hashes(root_directory, changed_dockerfiles_file)
